ee97cacf73eae76f6d5b83c4ce41b3c31cad592b
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2022-06-25 21:00:02 +0530

    fix: updates for breaking changes to ffi api (#4)

--- src/winapi.ts
+++ src/winapi.ts
@@ -1,121 +1,124 @@
 if (Deno.build.os === "windows") {
   // deno-lint-ignore no-var no-inner-declarations
-  var lib = Deno.dlopen("C:\\Windows\\System32\\Kernel32.dll", {
-    // https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea
-    CreateFileA: {
-      nonblocking: true,
-      parameters: [
-        "pointer", /* lpFileName */
-        "u32", /* dwDesiredAccess */
-        "u32", /* dwShareMode */
-        "pointer", /* lpSecurityAttributes */
-        "u32", /* dwCreationDisposition */
-        "u32", /* dwFlagsAndAttributes */
-        "pointer", /* hTemplateFile */
-      ],
-      result: "pointer",
-    },
+  var lib = Deno.dlopen(
+    "C:\\Windows\\System32\\Kernel32.dll",
+    {
+      // https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea
+      CreateFileA: {
+        nonblocking: true,
+        parameters: [
+          "pointer", /* lpFileName */
+          "u32", /* dwDesiredAccess */
+          "u32", /* dwShareMode */
+          "pointer", /* lpSecurityAttributes */
+          "u32", /* dwCreationDisposition */
+          "u32", /* dwFlagsAndAttributes */
+          "pointer", /* hTemplateFile */
+        ],
+        result: "pointer",
+      },
 
-    // https://docs.microsoft.com/en-us/windows/win32/api/namedpipeapi/nf-namedpipeapi-peeknamedpipe
-    PeekNamedPipe: {
-      parameters: [
-        "pointer", /* hNamedPipe */
-        "pointer", /* lpBuffer */
-        "u32", /* nBufferSize */
-        "pointer", /* lpBytesRead */
-        "pointer", /* lpTotalBytesAvail */
-        "pointer", /* lpBytesLeftThisMessage */
-      ],
-      result: "i32",
-    },
+      // https://docs.microsoft.com/en-us/windows/win32/api/namedpipeapi/nf-namedpipeapi-peeknamedpipe
+      PeekNamedPipe: {
+        parameters: [
+          "pointer", /* hNamedPipe */
+          "pointer", /* lpBuffer */
+          "u32", /* nBufferSize */
+          "pointer", /* lpBytesRead */
+          "pointer", /* lpTotalBytesAvail */
+          "pointer", /* lpBytesLeftThisMessage */
+        ],
+        result: "i32",
+      },
 
-    // https://docs.microsoft.com/en-us/windows/win32/api/handleapi/nf-handleapi-closehandle
-    CloseHandle: {
-      parameters: ["pointer"],
-      result: "i32",
-    },
+      // https://docs.microsoft.com/en-us/windows/win32/api/handleapi/nf-handleapi-closehandle
+      CloseHandle: {
+        parameters: ["pointer"],
+        result: "i32",
+      },
 
-    // https://docs.microsoft.com/en-us/windows/win32/api/namedpipeapi/nf-namedpipeapi-disconnectnamedpipe
-    DisconnectNamedPipe: {
-      parameters: ["pointer"],
-      result: "i32",
-    },
+      // https://docs.microsoft.com/en-us/windows/win32/api/namedpipeapi/nf-namedpipeapi-disconnectnamedpipe
+      DisconnectNamedPipe: {
+        parameters: ["pointer"],
+        result: "i32",
+      },
 
-    // https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-writefile
-    WriteFile: {
-      // nonblocking: true,
-      parameters: [
-        "pointer", /* hFile */
-        "pointer", /* lpBuffer */
-        "u32", /* nNumberOfBytesToWrite */
-        "pointer", /* lpNumberOfBytesWritten */
-        "pointer", /* lpOverlapped */
-      ],
-      result: "i32",
-    },
+      // https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-writefile
+      WriteFile: {
+        // nonblocking: true,
+        parameters: [
+          "pointer", /* hFile */
+          "pointer", /* lpBuffer */
+          "u32", /* nNumberOfBytesToWrite */
+          "pointer", /* lpNumberOfBytesWritten */
+          "pointer", /* lpOverlapped */
+        ],
+        result: "i32",
+      },
 
-    // https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-readfile
-    ReadFile: {
-      // In Overlapped IO, Read returns immediately, and the completion
-      // is signaled by the OVERLAPPED structure.
-      // nonblocking: true,
-      parameters: ["pointer", "pointer", "u32", "pointer", "pointer"],
-      result: "i32",
-    },
+      // https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-readfile
+      ReadFile: {
+        // In Overlapped IO, Read returns immediately, and the completion
+        // is signaled by the OVERLAPPED structure.
+        // nonblocking: true,
+        parameters: ["pointer", "pointer", "u32", "pointer", "pointer"],
+        result: "i32",
+      },
 
-    // https://docs.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-createnamedpipea
-    CreateNamedPipeA: {
-      // nonblocking: true,
-      parameters: [
-        "pointer", /* lpName */
-        "u32", /* dwOpenMode */
-        "u32", /* dwPipeMode */
-        "u32", /* nMaxInstances */
-        "u32", /* nOutBufferSize */
-        "u32", /* nInBufferSize */
-        "u32", /* nDefaultTimeOut */
-        "pointer", /* lpSecurityAttributes */
-      ],
-      result: "pointer",
-    },
+      // https://docs.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-createnamedpipea
+      CreateNamedPipeA: {
+        // nonblocking: true,
+        parameters: [
+          "pointer", /* lpName */
+          "u32", /* dwOpenMode */
+          "u32", /* dwPipeMode */
+          "u32", /* nMaxInstances */
+          "u32", /* nOutBufferSize */
+          "u32", /* nInBufferSize */
+          "u32", /* nDefaultTimeOut */
+          "pointer", /* lpSecurityAttributes */
+        ],
+        result: "pointer",
+      },
 
-    // https://docs.microsoft.com/en-us/windows/win32/api/namedpipeapi/nf-namedpipeapi-connectnamedpipe
-    ConnectNamedPipe: {
-      nonblocking: true,
-      parameters: ["pointer", "pointer"],
-      result: "i32",
-    },
+      // https://docs.microsoft.com/en-us/windows/win32/api/namedpipeapi/nf-namedpipeapi-connectnamedpipe
+      ConnectNamedPipe: {
+        nonblocking: true,
+        parameters: ["pointer", "pointer"],
+        result: "i32",
+      },
 
-    // https://docs.microsoft.com/en-us/windows/win32/api/ioapiset/nf-ioapiset-getoverlappedresult
-    GetOverlappedResult: {
-      nonblocking: true,
-      parameters: ["pointer", "pointer", "pointer", "u8"],
-      result: "i32",
-    },
+      // https://docs.microsoft.com/en-us/windows/win32/api/ioapiset/nf-ioapiset-getoverlappedresult
+      GetOverlappedResult: {
+        nonblocking: true,
+        parameters: ["pointer", "pointer", "pointer", "u8"],
+        result: "i32",
+      },
 
-    CancelIoEx: {
-      parameters: ["pointer", "pointer"],
-      result: "i32",
-    },
+      CancelIoEx: {
+        parameters: ["pointer", "pointer"],
+        result: "i32",
+      },
 
-    GetLastError: {
-      parameters: [],
-      result: "u32",
-    },
+      GetLastError: {
+        parameters: [],
+        result: "u32",
+      },
 
-    FormatMessageA: {
-      parameters: [
-        "u32",
-        "pointer",
-        "u32",
-        "u32",
-        "pointer",
-        "u32",
-        "pointer",
-      ],
-      result: "i32",
-    },
-  });
+      FormatMessageA: {
+        parameters: [
+          "u32",
+          "pointer",
+          "u32",
+          "u32",
+          "pointer",
+          "u32",
+          "pointer",
+        ],
+        result: "i32",
+      },
+    } as const,
+  );
 }
 
 function checkSupported() {
@@ -154,7 +157,7 @@ export const PIPE_ACCEPT_REMOTE_CLIENTS = 0x00000000;
 export const PIPE_REJECT_REMOTE_CLIENTS = 0x00000008;
 export const PIPE_UNLIMITED_INSTANCES = 255;
 
-export type HANDLE = Deno.UnsafePointer;
+export type HANDLE = bigint;
 
 export async function CreateFileA(
   lpFileName: string,
@@ -168,12 +171,12 @@ export async function CreateFileA(
     cstr(lpFileName),
     dwDesiredAccess >>> 0,
     dwShareMode >>> 0,
-    new Deno.UnsafePointer(0n),
+    0n,
     dwCreationDisposition >>> 0,
     dwFlagsAndAttributes >>> 0,
-    new Deno.UnsafePointer(0n),
+    0n,
   )) as HANDLE;
-  if (handle.value === BigInt(INVALID_HANDLE_VALUE)) {
+  if (handle === BigInt(INVALID_HANDLE_VALUE)) {
     throw new Error(`CreateFileA failed`);
   }
   return handle;
@@ -216,7 +219,7 @@ export function WriteFile(
       hFile,
       lpBuffer,
       nNumberOfBytesToWrite,
-      new Deno.UnsafePointer(0n),
+      0n,
       lpOverlapped,
     ),
     [997],
@@ -235,7 +238,7 @@ export function ReadFile(
       hFile,
       lpBuffer,
       nNumberOfBytesToRead,
-      new Deno.UnsafePointer(0n),
+      0n,
       lpOverlapped,
     ),
     [997],
@@ -315,9 +318,9 @@ export function CreateNamedPipeA(
     nOutBufferSize >>> 0,
     nInBufferSize >>> 0,
     nDefaultTimeOut >>> 0,
-    new Deno.UnsafePointer(0n),
+    0n,
   ) as HANDLE;
-  if (handle.value === BigInt(INVALID_HANDLE_VALUE)) {
+  if (handle === BigInt(INVALID_HANDLE_VALUE)) {
     throw new Error(`CreateNamedPipeA failed`);
   }
   return handle;
@@ -367,7 +370,7 @@ export function FormatMessage(errCode: number): string {
   ) as number;
 
   const lpBufferView = new Deno.UnsafePointerView(
-    new Deno.UnsafePointer(lpBufferPtr[0]),
+    lpBufferPtr[0],
   );
   return lpBufferView.getCString();
 }
